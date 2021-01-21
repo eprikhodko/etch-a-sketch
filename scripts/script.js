93d21576fd06f93cafef0a0b1bbb362e91fc50e0
Evgeny Prikhodko <eprikhodkodev@gmail.com>
2021-01-21 16:28:55 +0300

    Add working v 0.1 etch a sketch

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,45 @@
- //select a grid-container div
- let gridContainer = document.getElementById("grid-container");
+//select a grid-container div
+let gridContainer = document.getElementById("grid-container");
 
 //create a new div element (Create a <div> node)
- let gridElement = document.createElement("div");
+let gridElement = document.createElement("div");
+
 //  set class name
- gridElement.className = "grid-element";
- //create a text node
-//  let textNode = document.createTextNode("grid element");
- //append the text to the div element
-//  gridElement.appendChild(textNode);
+gridElement.className = "grid-element";
+
+
+// append this single elements to the parent grid container div
+for (let i = 0; i < 256; i++) {
+    //append li element to the unordered list
+    // console.log("loop is working");
+    gridContainer.appendChild(gridElement.cloneNode(true));
+}
+
+// select all grid elements
+let allGridElements = document.querySelectorAll('.grid-element');
+
+allGridElements.forEach(item => {
+    item.addEventListener("mouseenter", event => {
+      //  add event listener
+ 
+    console.log("click!");
+    item.className = "grid-element-black";
+  
+    })
+  })
+
+// let gridElementTest = document.getElementById("grid-element-test");
+
+// gridElementTest.addEventListener("mouseenter", function() {
+//     console.log("click!");
+//     gridElementTest.className = "grid-element-black";
+//   })
 
 
 //  gridContainer.appendChild(gridElement);
 // gridContainer.appendChild(gridElement.cloneNode(true));
 
-
-for (let i = 0; i < 256; i++) {
-    //append li element to the unordered list
-    console.log("loop is working");
-    gridContainer.appendChild(gridElement.cloneNode(true));
-}
+ //create a text node
+//  let textNode = document.createTextNode("grid element");
+ //append the text to the div element
+//  gridElement.appendChild(textNode);
